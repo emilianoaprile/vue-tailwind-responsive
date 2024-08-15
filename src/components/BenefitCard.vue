0b59e518928607c8b5fb06f1ae402ff205c90292
EmilianoAprile <emilianoaprile77@icloud.com>
2024-08-15 16:06:54 +0200

    add > buttons

--- src/components/BenefitCard.vue
+++ src/components/BenefitCard.vue
@@ -3,6 +3,10 @@
         <div class="number text-6xl font-extrabold text-[#9593E6] mb-4">{{ benefits.number }}</div>
         <h1 class="title text-3xl font-extrabold text-[#9593E6] mb-8">{{ benefits.title }}</h1>
         <p>{{ benefits.text }}</p>
+        <button
+            class="px-6 py-2 text-xl font-light rounded-3xl cursor-pointer bg-gradient-to-r from-blue-500 from-10% to-[#9593E6] mt-8 text-white">
+            {{ benefits.btn_text }}
+        </button>
     </div>
 </template>
 
